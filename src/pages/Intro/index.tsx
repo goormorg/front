a179bc8e7f1269c3ef9cb1c488523afd2012dcad
Lee-Seungje <moondgod@gmail.com>
2024-01-27 17:30:59 +0900

    Add intro

--- src/pages/Intro/index.tsx
+++ src/pages/Intro/index.tsx
@@ -1,5 +1,9 @@
 import * as S from "./style";
 
+import { Button } from "components/Button";
+
+import { css } from "@emotion/react";
+
 const Intro = () => (
   <S.Wrapper>
     <S.Title>
@@ -8,8 +12,14 @@ const Intro = () => (
       식단을 찾아봐요
     </S.Title>
     <S.Text>what’s up healthup</S.Text>
-    <S.Button>로그인</S.Button>
-    <S.Button>회원가입</S.Button>
+    <S.ButtonWrapper>
+      <Button onClick={() => alert("test")} color="grey">
+        로그인
+      </Button>
+      <Button onClick={() => alert("test")} color="green">
+        회원가입
+      </Button>
+    </S.ButtonWrapper>
   </S.Wrapper>
 );
 
--- src/pages/Intro/style.ts
+++ src/pages/Intro/style.ts
@@ -1,11 +1,13 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  width: 360px;
+  width: 312px;
   height: 640px;
   display: flex;
   align-items: center;
   flex-direction: column;
+  padding: 0 24px;
+  position: relative;
 `;
 
 export const Title = styled.h1`
@@ -13,6 +15,7 @@ export const Title = styled.h1`
   text-align: center;
   font-size: 36px;
   font-weight: 900;
+  margin: 215px 0 8px;
 `;
 
 export const Text = styled.span`
@@ -32,4 +35,13 @@ export const Button = styled.button`
   font-size: 16px;
   font-weight: 400;
   letter-spacing: -0.48px;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 312px;
+  position: absolute;
+  bottom: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
 `;
