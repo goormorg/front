706e56ed5c933aa0c8b9c27e9a7b927324220233
Lee-Seungje <moondgod@gmail.com>
2024-01-28 00:57:04 +0900

    Add getMyInfo

--- src/pages/My.tsx
+++ src/pages/My.tsx
@@ -1,14 +1,26 @@
 /** @jsxImportSource @emotion/react */
 
 import React from "react";
-import { css } from '@emotion/react'
+import { css } from "@emotion/react";
+
+import axios from "axios";
 
 function MyPage() {
-    return (
-        <>
-        <p>my</p>
-        </>
-    )
+  const userEmail = "moondgod@gmail.com";
+
+  const url =
+    process.env.REACT_APP_BASE_URL + "/v1/api/user/detail?email=" + userEmail;
+
+  const getMyInfo = async () => {
+    const { data } = await axios.get(url);
+    console.log(data);
+  };
+
+  return (
+    <>
+      <p onClick={getMyInfo}>my</p>
+    </>
+  );
 }
 
-export { MyPage }
+export { MyPage };
