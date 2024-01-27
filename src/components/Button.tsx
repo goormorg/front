ad8cb473c8ab76f5f20eab6b8a524c9cb25391e6
Dipokal HHJ <hhj@devent.kr>
2024-01-27 14:57:46 +0900

    feat: add button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,49 +7,37 @@ type ButtonType = {
     children?: any
     onClick?: any
     disable?: boolean
-    size?: string
-    borderColor?: string
-    type?: string
-    stylecss?: object
+    color?: "light" | "blue"
 
 }
 
-function Button({ children, onClick, disable}: ButtonType) {
-    const buttonSizeValue = {
-        "sm": {
-            padding: "0.4rem 0.9rem",
-            fontSize: "0.8rem"
+function Button({ children, onClick, disable, color = 'light' }: ButtonType) {
+    const buttonColor: any = {
+        "light": {
+            "backgroundColor": "#E8EAEE",
+            "color": "#242427",
+            "hoverBackgroundColor": "#d7d9de"
         },
-        "md": {
-            padding: "0.6rem 1.4rem",
-            fontSize: "1rem"
+        "blue": {
+            "backgroundColor": "#266DF6",
+            "color": "#FBFBFD",
+            "hoverBackgroundColor": "#205ed6"
         },
-        "lg": {
-            padding: "0.6rem 3.4rem",
-            fontSize: "1rem"
-        },
-    }
-
-    const buttonBorderColorValue = {
-        "default": "#25252B",
-        "green": "#43AB5A",
-        "red": "#D73131"
-    }
-
-    const buttonRoundTypeValue = {
-        "round": "0.6rem",
-        "circle": "10rem"
     }
 
     return (
         <button onClick={onClick} disabled={disable} css={css({ 
             fontFamily: "'Noto Sans KR', sans-serif",
-
-            backgroundColor: "#121215",
-            color: "#888A93",
+            outline: "none",
+            border: "none",
+            padding: "0.8rem 1.4rem",
+            borderRadius: "0.6rem",
+            backgroundColor: buttonColor[color].backgroundColor,
+            color: buttonColor[color].color,
+            transition: "0.1s",
             ":hover": {
-                backgroundColor: "#18181c",
-                color: "#93959e",
+                backgroundColor: buttonColor[color].hoverBackgroundColor,
+                color: buttonColor[color].color,
             }
         })}>
             {children}
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,11 +2,15 @@
 
 import React from "react";
 import { css } from '@emotion/react'
+import { Button } from "../components/Button";
 
 function MainPage() {
     return (
         <>
         <p>main</p>
+        <Button color="light">버튼</Button>
+        <Button color="blue">버튼</Button>
+
         </>
     )
 }
