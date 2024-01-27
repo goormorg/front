eedf4718d8c7097788a73c737565f0add9bb7d5e
Dipokal HHJ <hhj@devent.kr>
2024-01-27 16:23:09 +0900

    feat: add search item

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -6,12 +6,31 @@ import { Input } from "../components/Input";
 import { Container } from "../components/Container";
 import { Navbar } from "../components/Navbar";
 
+
+
+type RoadmapItemType = {
+    title?: string
+    description?: string
+    date?: string
+    tag?: string[]
+}
+
 function SearchPage() {
     const [searchValue, setSearchValue] = useState("")
+    const [item, setItem] = useState([
+        {
+            title: "dldld",
+            description: "dfsdef",
+            date: "1010-10-10",
+            tag: ["sss", "sdgvsde"]
+        }
+    ])
 
     const onChangeSearch = (e: any) => {
         setSearchValue(e.target.value)
     }
+
+
     
     return (
         <>
@@ -26,13 +45,44 @@ function SearchPage() {
                     <Input value={searchValue} onChange={onChangeSearch} placeholder={"검색..."}></Input>
                 </div>
 
-                
+                <div css={css({
+                    display: "flex",
+                    paddingTop: "4rem",
+                    width: "100%"
+                })}>
+                    {item.map(el => (
+                        <RoadmapItem 
+                        title={el.title} 
+                        description={el.description}
+                        date={el.date}
+                        tag={el.tag}
+                        ></RoadmapItem>
+                    ))}
+
+                </div>
             </Container>
 
                 
             <Navbar></Navbar>
 
         </>
+    )
+}
+
+
+function RoadmapItem({ title, description, date, tag }: RoadmapItemType) {
+    return (
+        <div css={css({
+            borderRadius: "0.6rem",
+            border: "0.1rem solid #E8EAEE",
+            padding: "1rem",
+            display: "flex",
+            flexDirection: "column",
+            width: "100%"
+        })}>
+            <h3 css={css({ marginTop: 0 })}>{title}</h3>
+            <p>{description}</p>
+        </div>
     )
 }
 
