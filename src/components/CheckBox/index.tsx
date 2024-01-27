c01520c84bf774a778fb6bcf4ee48f3268928322
Lee-Seungje <moondgod@gmail.com>
2024-01-27 20:25:51 +0900

    Fix border

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -20,13 +20,13 @@ const CheckBox: React.FC<Props> = ({
         {isChecked && (
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="15"
-            height="15"
-            viewBox="0 0 15 15"
+            width="17"
+            height="17"
+            viewBox="0 0 17 17"
             fill="none"
           >
-            <rect x="0.5" y="0.5" width="14" height="14" stroke="#42BD4E" />
-            <path d="M3 7L6 10L12 4" stroke="#42BD4E" />
+            <rect x="0.5" y="0.5" width="16" height="16" stroke="#42BD4E" />
+            <path d="M4 7L6 11L12 6" stroke="#42BD4E" />
           </svg>
         )}
       </S.CheckBox>
