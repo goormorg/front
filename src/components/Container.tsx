d3045908230ebd40d63a0d54c51eb44d0819b480
Dipokal HHJ <hhj@devent.kr>
2024-01-27 15:00:16 +0900

    feat: add container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -9,8 +9,22 @@ type ContainerType = {
 
 function Container({ children }: ContainerType) {
     return (
-        <>
-        </>
+        <div css={css({
+            display: "flex",
+            justifyContent: "center",
+            width: "100%"
+        })}>
+            <div css={css({
+                display: "flex",
+                width: "100%", 
+                padding: "1rem 1rem",
+                '@media(min-width: 1200px)': {
+                    maxWidth: "1200px",
+                }
+            })}>
+                {children}
+            </div>
+        </div>
     )
 }
 
