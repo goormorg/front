de4ba8036b61ccc5c735f181c18931fa6a83baca
Dipokal HHJ <hhj@devent.kr>
2024-01-27 15:25:00 +0900

    feat: add input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,12 +5,25 @@ import { css } from '@emotion/react'
 
 type InputType = {
     children?: any
+    value?: any
+    onChange?: any
+    placeholder?: any
 }
 
-function Input({ children }: InputType) {
+function Input({ children, value, onChange, placeholder }: InputType) {
     return (
-        <>
-        </>
+        <input
+            value={value}
+            onChange={onChange}
+            placeholder={placeholder}
+            css={css({
+                border: "none",
+                outline: "none",
+                backgroundColor: "#E8EAEE",
+                borderRadius: "0.6rem",
+                padding: "0.6rem 0.8rem"
+            })}
+        ></input>
     )
 }
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,6 +3,7 @@ import { css } from '@emotion/react'
 import { Button } from "../components/Button";
 import { Container } from "../components/Container";
 import { Navbar } from "../components/Navbar";
+import { Input } from "../components/Input";
 
 function MainPage() {
     return (
@@ -11,6 +12,8 @@ function MainPage() {
         <p>main</p>
         <Button color="light">버튼</Button>
         <Button color="blue">버튼</Button>
+
+        <Input></Input>
 
         </Container>
         <Navbar></Navbar>
